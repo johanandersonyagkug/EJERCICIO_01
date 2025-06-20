c3710c0e0b12c6b0a3bd3ef3132bc8d32dd5f582
johan <77202059@idexperujapon.edu.pe>
2025-06-20 17:57:34 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("ingrese el primer num");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo num");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            int suma = n1 + n2;
+            Console.WriteLine("la respuesta es: ");
+            
+        }
+    }
+}
